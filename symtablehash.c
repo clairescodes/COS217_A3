dddba196d446cd3ea765394827e0f644fd12afdb
Jeonghyun Shin <js9523@princeton.edu>
2024-10-25 02:20:23 -0400

    edited symtablehash.c

--- symtablehash.c
+++ symtablehash.c
@@ -30,3 +30,38 @@ static size_t SymTable_hash(const char *pcKey, size_t uBucketCount) {
 
     return uHash % uBucketCount;
 }
+
+SymTable_T SymTable_new(void) {
+   SymTable_T oSymTable;
+   size_t i; 
+
+
+   oSymTable = malloc(sizeof(struct SymTable));
+   if (oSymTable == NULL)
+    return NULL;
+   
+   oSymTable->buckets = 
+
+
+    
+}
+
+void SymTable_free(SymTable_T oSymTable);
+
+size_t SymTable_getLength(SymTable_T oSymTable);
+
+int SymTable_put(SymTable_T oSymTable,
+    const char *pcKey, const void *pvValue);
+
+void *SymTable_replace(SymTable_T oSymTable,
+    const char *pcKey, const void *pvValue);
+
+int SymTable_contains(SymTable_T oSymTable, const char *pcKey);
+
+void *SymTable_get(SymTable_T oSymTable, const char *pcKey);
+
+void *SymTable_remove(SymTable_T oSymTable, const char *pcKey);
+
+void SymTable_map(SymTable_T oSymTable,
+    void (*pfApply)(const char *pcKey, void *pvValue, void *pvExtra),
+    const void *pvExtra);
