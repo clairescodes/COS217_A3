23fc159bb702d5c2d8b5009124abbaf99b9557df
Jeonghyun Shin <js9523@princeton.edu>
2024-10-28 23:34:24 -0400

    added comments

--- symtable.h
+++ symtable.h
@@ -5,24 +5,47 @@
 
 typedef struct SymTable *SymTable_T;
 
+/* creates a empty SymTable, allocates memory for it, 
+   and returns it */
 SymTable_T SymTable_new(void);
 
+/* frees memory needed for the SymTable */
 void SymTable_free(SymTable_T oSymTable);
 
+/* returns number of bindings in oSymTable */
 size_t SymTable_getLength(SymTable_T oSymTable);
 
+/* adds new binding to symbol table (oSymTable) if the key pcKey 
+   doesn't exist in oSymTable. 
+   returns 1 if binding was added, returns 0 if memory allocation 
+   fails or key already exists in oSymTable */
 int SymTable_put(SymTable_T oSymTable,
     const char *pcKey, const void *pvValue);
 
+/* replace old value (pvOldValue) of key pcKey in oSymTable 
+    with new value pvValue and returns pvOldValue. 
+    if the given pcKey doesn't exist within oSymTable, 
+    returns NULL */
 void *SymTable_replace(SymTable_T oSymTable,
     const char *pcKey, const void *pvValue);
 
+/* checks if given key pcKey exists within oSymTable. 
+    returns 1 if pcKey exists, if else returns 0 */
 int SymTable_contains(SymTable_T oSymTable, const char *pcKey);
 
+/* returns value associated with key pcKey if it exists 
+    within oSymTable, returns NULL if can't find given pcKey
+    in oSymTable */
 void *SymTable_get(SymTable_T oSymTable, const char *pcKey);
 
+/* removes binding associated with given pcKey from the
+   symbol table (oSymTable) and frees memory.
+   return value associated with removed key if key existed
+   in oSymTable. return NULL otherwise. */
 void *SymTable_remove(SymTable_T oSymTable, const char *pcKey);
 
+/* to each binding in oSymTable, apply function (pfApply) given by
+   the user. */
 void SymTable_map(SymTable_T oSymTable,
     void (*pfApply)(const char *pcKey, void *pvValue, void *pvExtra),
     const void *pvExtra);
